8c4b30e9323914d1f38fd34a9a654a8a85b469b7
Lauren Girolmetti <laurengiro@gmail.com>
2019-10-29 19:12:13 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def min_max_diff():
+    the_array = [15, 22, 84, 14, 88, 23]
+    min_val = the_array[0]
+    max_val = the_array[0]
+
+    for i in range(len(the_array)):
+        current_value = the_array[i]
+        if current_value < min_val:
+            min_val = current_value
+        elif current_value > max_val:
+            max_val = current_value
+
+    print("the_highest_value = {0}".format(max_val))
+    print("the_smallest_number = {0}".format(min_val))
+    print("the_difference = {0}".format(max_val - min_val))
